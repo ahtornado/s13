2140e8b7273f0682d69787c519f1466709a37f82
ahtornado <114653379@qq.com>
2017-05-31 21:14:29 +0800

    111

--- day5/guess_num.py
+++ day5/guess_num.py
@@ -3,9 +3,20 @@
 # Author:Alvin.xie
 import random
 retry_count = 0
+#随机产生一个10以内的数
 real_num =random.randrange(10)
 while retry_count < 3:
-    guess_num=int (raw_input("Please guess the real num: "))
+    #去掉空格和回车
+    guess_num=raw_input("Please guess the real num: ").strip()
+    #判断长度是否为 0
+    if len(guess_num) == 0:
+        continue
+    #判断是否为数字
+    if guess_num.isdigit():
+        guess_num = int(guess_num)
+    else:
+        print "You need input num"
+        continue
     if guess_num > real_num:
         print "Wrong ! you need try smaller!"
     elif guess_num < real_num:
